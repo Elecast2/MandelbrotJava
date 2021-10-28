d4389ba1cbdf50c6b926c1b0f1fb4468f2f9b579
Diego Castellano <Diego Castellano@DESKTOP-FE9TU6O>
2021-10-27 23:03:23 -0300

    remove duplicated code

--- src/net/minemora/mandelbrotjava/MandelbrotJava.java
+++ src/net/minemora/mandelbrotjava/MandelbrotJava.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.Timer;
 
 import javax.imageio.ImageIO;
+import javax.swing.JProgressBar;
 
 public class MandelbrotJava {
 	
@@ -32,58 +33,11 @@ public class MandelbrotJava {
 	}
 
 	public static void renderMandel(int[][] mandel) {
-		int black = 0;
-		int[] colors = new int[iterations];
-		for (int i = 0; i < iterations; i++) {
-			colors[i] = Color.HSBtoRGB(i / 256f, 1, i / (i + 8f));
-		}
-		for (int row = 0; row < size; row++) {
-			for (int col = 0; col < size; col++) {
-				double c_re = (col - size / 2) * 4.0 / size;
-				double c_im = (row - size / 2) * 4.0 / size;
-				double x = 0, y = 0;
-				int iteration = 0;
-				while (x * x + y * y < 4 && iteration < iterations) {
-					double x_new = x * x - y * y + c_re;
-					y = 2 * x * y + c_im;
-					x = x_new;
-					iteration++;
-				}
-				if (iteration < iterations)
-					mandel[col][row] = colors[iteration];
-				else
-					mandel[col][row] = black;
-			}
-		}
+		renderMandel(mandel, size, iterations, 0, 0, false, false, null, null);
 	}
-
+	
 	public static void renderMandel(int[][] mandel, double re, double im) {
-		int black = 0;
-		int[] colors = new int[iterations];
-		for (int i = 0; i < iterations; i++) {
-			colors[i] = Color.HSBtoRGB(i / 256f, 1, i / (i + 8f));
-		}
-		for (int row = 0; row < size; row++) {
-			for (int col = 0; col < size; col++) {
-				double c_re = (col - size / 2) * 4.0 / size;
-				double c_im = (row - size / 2) * 4.0 / size;
-				double x = 0, y = 0;
-				double x_new2 = x * x - y * y + c_re;
-				y = 2 * x * y + c_im;
-				x = x_new2;
-				int iteration = 0;
-				while (x * x + y * y < 4 && iteration < iterations) {
-					double x_new = x * x - y * y + re;
-					y = 2 * x * y + im;
-					x = x_new;
-					iteration++;
-				}
-				if (iteration < iterations)
-					mandel[col][row] = colors[iteration];
-				else
-					mandel[col][row] = black;
-			}
-		}
+		renderMandel(mandel, size, iterations, re, im, true, false, null, null);
 	}
 	
 	public static void renderMandelPNG(int size, int iterations) {
@@ -93,32 +47,7 @@ public class MandelbrotJava {
         frame.getProgressBar().setString("");
         frame.getProgressBar().update(frame.getProgressBar().getGraphics());
         BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
-        int black = 0;
-        int[] colors = new int[iterations]; 
-        for (int i = 0; i<iterations; i++) {
-            colors[i] = Color.HSBtoRGB(i/256f, 1, i/(i+8f));
-        }
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col++) {
-            	double c_re = (col - size / 2) * 4.0 / size;
-				double c_im = (row - size / 2) * 4.0 / size;
-				double x = 0, y = 0;
-				double x_new2 = x * x - y * y + c_re;
-				y = 2 * x * y + c_im;
-				x = x_new2;
-				int iteration = 0;
-				while (x * x + y * y < 4 && iteration < iterations) {
-					double x_new = x * x - y * y + lastRe;
-					y = 2 * x * y + lastIm;
-					x = x_new;
-					iteration++;
-				}
-                if (iteration < iterations) image.setRGB(col, row, colors[iteration]);
-                else image.setRGB(col, row, black);
-            }
-            frame.getProgressBar().setValue(row);
-            frame.getProgressBar().update(frame.getProgressBar().getGraphics());
-        }
+        renderMandel(null, size, iterations, lastRe, lastIm, true, true, image, frame.getProgressBar());
         try {
 			ImageIO.write(image, "png", new File("mandel.png"));
 		} catch (IOException e) {
@@ -127,5 +56,53 @@ public class MandelbrotJava {
         frame.getProgressBar().setValue(0);
         frame.getProgressBar().setString("Done!");
         frame.getProgressBar().update(frame.getProgressBar().getGraphics());
+	}
+
+	public static void renderMandel(int[][] mandel, int size, int iterations, double re, double im, boolean staticC, boolean toPng, BufferedImage image, JProgressBar bar) {
+		int[] colors = new int[iterations];
+		for (int i = 0; i < iterations; i++) {
+			colors[i] = Color.HSBtoRGB(i / 256f, 1, i / (i + 8f));
+		}
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				double c_re = (col - size / 2) * 4.0 / size;
+				double c_im = (row - size / 2) * 4.0 / size;
+				double x = 0, y = 0;
+				if(staticC) {
+					double x_new2 = x * x - y * y + c_re;
+					y = 2 * x * y + c_im;
+					x = x_new2;
+				}
+				int iteration = 0;
+				if(staticC) {
+					while (x * x + y * y < 4 && iteration < iterations) {
+						double x_new = x * x - y * y + re;
+						y = 2 * x * y + im;
+						x = x_new;
+						iteration++;
+					}
+				}
+				else {
+					while (x * x + y * y < 4 && iteration < iterations) {
+						double x_new = x * x - y * y + c_re;
+						y = 2 * x * y + c_im;
+						x = x_new;
+						iteration++;
+					}
+				}
+				if(toPng) {
+					if (iteration < iterations) image.setRGB(col, row, colors[iteration]);
+	                else image.setRGB(col, row, 0);
+				}
+				else {
+					if (iteration < iterations) mandel[col][row] = colors[iteration];
+					else mandel[col][row] = 0;
+				}
+			}
+			if(toPng) {
+				bar.setValue(row);
+	            bar.update(frame.getProgressBar().getGraphics());
+			}
+		}
 	}
 }
